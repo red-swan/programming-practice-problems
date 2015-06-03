2cea663fea2236b97e568f4f73abd9d181f42979
jaredswan <jaredswan@outlook.com>
2015-06-02 20:08:08 -0700

    minor changes

--- F-Sharp/Unsolved/036.fsx
+++ F-Sharp/Unsolved/036.fsx
@@ -3,3 +3,9 @@
 Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
 
 (Please note that the palindromic number, in either base, may not include leading zeros.) *)
+
+
+
+
+let decdigits number =  [for c in (string number) -> c] |> List.map (fun x -> (int x) - (int '0'))
+
