293805c75ce2969a9bdeee19058fcc5d2966c164
Jared Swan <red-swan@users.noreply.github.com>
2015-06-05 18:29:31 -0700

    Update 038.fsx

--- F-Sharp/Unsolved/038.fsx
+++ F-Sharp/Unsolved/038.fsx
@@ -11,3 +11,41 @@ The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5
 
 What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an
 integer with (1,2, ... , n) where n > 1?*)
+
+
+
+
+
+
+
+
+
+let rec distribute e = function
+    | [] -> [[e]]
+    | x::xs' as xs -> (e::xs)::[for xs in distribute e xs' -> x::xs]
+
+let rec permute = function
+    | [] -> [[]]
+    | e::xs -> List.collect (distribute e) (permute xs)
+
+let digits number = [for k in (string number) -> k]
+let digitalproduct number integers = List.map (fun x -> (number *x).ToString()) integers |> Seq.reduce (+)
+let ispandigital stringofdigits = (List.sort ([for j in (string stringofdigits) -> j])) = ['1';'2';'3';'4';'5';'6';'7';'8';'9']
+
+
+let ispandigitalproduct number = 
+    true
+
+let answer = 
+    digits 987654321
+    |> permute
+    |> List.sort
+    |> List.rev
+    |> List.find ispandigitalproduct
+
+let rec digitstonumber (listofdigits : char list) = 
+    match listofdigits with
+    | [] -> 0
+    | head::tail -> (int (10. ** (float (List.length listofdigits) - 1.) * float (int (head) - 48)) + (float ( digitstonumber tail))
+
+
