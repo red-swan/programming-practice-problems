8bf9e27cb23e0b56e8ec8cfa5b1498c1fccadf3e
jaredswan <jaredswan@outlook.com>
2015-06-06 14:08:45 -0700

    moved

---
+++ F-Sharp/Unsolved/039.fsx
@@ -0,0 +1,9 @@
+﻿(*If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?*)
+
+
+
+let check a b c = [(float a) / 3. ;  (float b) / 4.; (float c) / 5.]
