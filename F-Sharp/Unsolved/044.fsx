523840807577f8abb185431cd3b80049a30cae12
jaredswan <jaredswan@outlook.com>
2015-06-08 06:34:55 -0700

    Created

---
+++ F-Sharp/Unsolved/044.fsx
@@ -0,0 +1,16 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+let pentagonalnumbers = Seq.unfold (fun x-> Some(((3UL*x-1UL)*x)/2UL,x+1UL)) 1UL
+let pentagonaldifferences = Seq.unfold (fun x-> Some(3UL*x + 1UL,x+1UL)) 1UL
+let ispentagonal (x : uint64) = ((sqrt (24.0* (float x)  + 1.0) + 1.0) / 6.0) % 1.0 = 0.0
+
+Seq.find ispentagonal pentagonaldifferences
