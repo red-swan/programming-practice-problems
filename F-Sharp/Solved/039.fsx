d55380f7596ec72bcfb9c903560169ae5ba5831f
Jared Swan <red-swan@users.noreply.github.com>
2015-06-13 08:43:43 -0700

    optimized

--- F-Sharp/Solved/039.fsx
+++ F-Sharp/Solved/039.fsx
@@ -5,7 +5,43 @@
 For which value of p ≤ 1000, is the number of solutions maximised?*)
 
 
+// redoing 39
 
+open System
+
+let gettriples s = 
+    let mapitout kormore m  =
+        let rec loop = function
+        | [] -> []
+        | k::tail -> [m;k-m;s/(2*m*k)] :: loop tail
+        loop kormore
+    let getabc [m;n;d] =
+        let m2 = m*m
+        let n2 = n*n
+        ((m2-n2)*d,2*m*n*d,(m2+n2)*d)
+    let ms = [1 .. int (sqrt (float s))] |> List.filter (fun x-> s/2 % x = 0)
+    let findks m = [m+1 .. 2*m-1] |> List.filter (fun k -> k%2=1 && (s/(2*m))%k=0 && (gcd k m) = 1)
+    let ks = ms |> List.map (fun m-> findks m)
+    let mnds = List.map2 (fun m k-> mapitout k m) ms ks |> List.collect (fun x->x)
+    List.map getabc mnds
+
+let stopwatch = new System.Diagnostics.Stopwatch()
+stopwatch.Start()
+let answer =
+    [3 .. 1000]
+    |> List.maxBy (fun x-> gettriples x |> List.length)
+stopwatch.Stop()
+System.Console.WriteLine(stopwatch.ElapsedMilliseconds)
+
+
+
+
+
+
+
+
+
+// OLD VERSION OLD VERSION OLD VERSION OLD VERSION OLD VERSION OLD VERSION OLD VERSION
 
 // triples (if any) for a single perimeter and a single a
 let getsolution perimeter a =
