77e02fa27a22e3f12c7e87fb536ff86ae3527398
jaredswan <jaredswan@outlook.com>
2015-06-13 10:38:37 -0700

    working

--- F-Sharp/Unsolved/050.fsx
+++ F-Sharp/Unsolved/050.fsx
@@ -10,3 +10,49 @@ Which prime, below one-million, can be written as the sum of the most consecutiv
 
 // start with large windows and add up, the decrease the window size it seems
 
+// Generates a list of all primes below limit
+let sieveOfAtkin limit =
+    // initialize the sieve
+    let sieve = Array.create (limit + 1) false
+    // put in candidate primes: 
+    // integers which have an odd number of
+    // representations by certain quadratic forms
+    let inline invCand n pred =
+        if n < limit && pred then sieve.[n] <- not sieve.[n] 
+    let sqrtLimit = int (sqrt (float limit))
+    for x = 1 to sqrtLimit do
+        for y = 1 to sqrtLimit do
+            let xPow2 = x * x
+            let yPow2 = y * y
+            let n = 4 * xPow2 + yPow2 in invCand n (let m = n % 12 in m = 1 || m = 5)
+            let n = 3 * xPow2 + yPow2 in invCand n (n % 12 = 7)
+            let n = 3 * xPow2 - yPow2 in invCand n (x > y && n % 12 = 11)
+    // eliminate composites by sieving
+    let rec eliminate n =
+        if n <= sqrtLimit 
+        then if sieve.[n]
+             then let nPow2 = n * n
+                  for k in nPow2 .. nPow2 .. limit do
+                      Array.set sieve k false
+             eliminate (n + 2)
+    eliminate 5
+    // Generate list from the sieve (backwards)
+    let rec generateList acc n =
+        if n >= 5 then generateList (if sieve.[n] then n :: acc else acc) (n - 1)
+        else acc
+    2 :: 3 :: (generateList [] limit)
+
+
+let primes = sieveOfAtkin 1000000
+
+let isprime x= Seq.exists ((=)x) primes
+
+
+// cap number / 2 is a good limit to check
+let findlgconssum capnumber = 
+    let listofprimes = sieveOfAtkin capnumber
+    let windows n = Seq.windowed n listofprimes |> Seq.map (fun x-> (Array.sum x)) |> Seq.filter (fun x-> x<capnumber && isprime x)
+    let windowsizes = Seq.unfold (fun n -> if n = 1 then None else Some(windows n, n-1 )) capnumber
+    windowsizes |> Seq.take 1
+
+
