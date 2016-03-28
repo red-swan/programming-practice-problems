132294a5b480274524d6d8d5483cb6d4bb5a3aa5
Jared Swan <jaredswan@outlook.com>
2016-03-27 19:36:35 -0700

    adds test line to 001 to test work upload to github

--- F-Sharp/Solved/001.fsx
+++ F-Sharp/Solved/001.fsx
@@ -2,7 +2,7 @@
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.*)
-
+// test if work computer can upload to github
 
 [1 .. 999]
     |> List.filter (fun x-> x%3=0 || x%5=0)
