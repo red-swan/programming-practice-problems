33eccc08fb6ad2fe8662bed7367dc39d924f9f1e
Red Swan <redswan@outlook.com>
2021-01-09 02:06:22 -0800

    Fix F# solution to 844
    Before, it was using Seq.zip to compare finished strings. This
    doesn't handle the case of comparing "ab" to "a" because Seq.zip
    truncates the longer sequence
    
    Therefore, we switched it to use Seq.compareWith instead, which will
    fail if either string is longer than the other.

--- 844-backspace-string-compare.fsx
+++ 844-backspace-string-compare.fsx
@@ -10,6 +10,7 @@ let sample6 = "a######", "a#"
 let sample7 = "###", "#"
 let sample8 = "bxj##tw", "bxo#j##tw"
 let sample9 = "xywrrmp", "xywrrmu#p"
+let sample10 = "xp", "p"
 
 let f (s : string) = 
     let rec loop skip str = 
@@ -17,28 +18,26 @@ let f (s : string) =
               elif Seq.head str = '#' then yield! loop (skip + 1) (Seq.tail str)
               elif 0 < skip then yield! loop (skip - 1) (Seq.tail str)
               else 
-                yield Seq.head str
-                yield! loop skip (Seq.tail str)
-                }
+                  yield Seq.head str
+                  yield! loop skip (Seq.tail str)
+        }
     loop 0 (Seq.rev s)
 
 
 let backspaceCompare (s : string) (t : string) = 
     (f s, f t)
-    ||> Seq.zip
-    |> Seq.map (fun (a,b) -> a = b)
-    |> Seq.contains false
-    |> not
+    ||> Seq.compareWith Operators.compare
+    |> ((=)0)
 
 
 
-sample1 ||> backspaceCompare // true
-sample2 ||> backspaceCompare //true
-sample3 ||> backspaceCompare //true
-sample4 ||> backspaceCompare //false
-sample5 ||> backspaceCompare //true
-sample6 ||> backspaceCompare //true
-sample7 ||> backspaceCompare //true
-sample8 ||> backspaceCompare //true
-sample9 ||> backspaceCompare //true
-
+sample1 ||> backspaceCompare  // true
+sample2 ||> backspaceCompare  // true
+sample3 ||> backspaceCompare  // true
+sample4 ||> backspaceCompare  // false
+sample5 ||> backspaceCompare  // false
+sample6 ||> backspaceCompare  // true
+sample7 ||> backspaceCompare  // true
+sample8 ||> backspaceCompare  // true
+sample9 ||> backspaceCompare  // true
+sample10 ||> backspaceCompare // false
