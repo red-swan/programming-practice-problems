6eacdfbd867c0fc3c50ad9c263c190db58b1d73c
red-swan <redswan@outlook.com>
2021-02-17 21:22:02 -0800

    Solve monarchy ztm question in F#
    Rather simple really

--- ztm/problems/monarchy.fsx
+++ ztm/problems/monarchy.fsx
@@ -1,4 +1,4 @@
-
+#time
 type Status = | Alive | Dead
 type Monarchy = 
     {
@@ -9,19 +9,56 @@ type Monarchy =
 
 
 module Monarchy =
-    let rec birth parent child monarchy : Monarchy = 
+    let create name = {Name = name; Status = Alive; Children = []}
+
+    let rec birth (child : string) (parent : string) monarchy : Monarchy = 
         if parent = monarchy.Name then
-            {monarchy with Children = List.append monarchy.Children [child]}
+            {monarchy with Children = List.append monarchy.Children [create child]}
         else
-            {monarchy with Children = List.map (birth parent child) monarchy.Children }
-    let death name monarchy : Monarchy = 
-        failwith "not implemented"
+            {monarchy with Children = List.map (birth child parent) monarchy.Children }
+    
+    let birthMany l m = 
+        List.fold (fun m (c,p) -> birth c p m) m l
+
+    let rec death name monarchy : Monarchy = 
+        if name = monarchy.Name then
+            {monarchy with Status = Dead}
+        else
+            {monarchy with Children = List.map (death name) monarchy.Children }
+    let deathMany l m = 
+        List.fold (fun m n -> death n m) m l
+
+
     let getOrderOfSuccession monarchy : string list = 
-        failwith "not implemented"
+        let rec loop x = 
+            seq {
+                if x.Status = Alive then
+                    yield x.Name
+                yield! Seq.collect loop x.Children
+            }
+        loop monarchy
+        |> Seq.toList
 
 
 
+let monarchy = Monarchy.create "Jake"
 
+let births = 
+    [
+        ("Catherine","Jake"); ("Jane","Catherine"); ("Farah","Jane")
+        ("Tom","Jake"); ("Celine","Jake"); ("Mark","Catherine")
+        ("Peter","Celine")
+    ]
+
+let monarchy2 = Monarchy.birthMany births monarchy
+
+Monarchy.getOrderOfSuccession monarchy2
+
+let deaths = ["Jake";"Jane"]
+
+let monarchy3 = Monarchy.deathMany deaths monarchy2
+
+Monarchy.getOrderOfSuccession monarchy3
 
 
 
