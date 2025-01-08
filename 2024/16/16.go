f34df896835e5fb63f7c269406e987f5be6d3a14
Red Swan <redswan@outlook.com>
2025-01-08 01:45:42 -0700

    Solve 2024-16.2 in go

--- 2024/16/16.go
+++ 2024/16/16.go
@@ -20,7 +20,6 @@ type Position struct {
 	x int
 	y int
 }
-
 type Heading struct {
 	position  Position
 	direction int
@@ -32,14 +31,19 @@ type GameSetup struct {
 	walls map[Position]bool
 }
 
-type SearchNode struct {
-	heading Heading
-	moves   uint
-	turns   uint
+type PathNode struct {
+	heading  Heading
+	moves    uint
+	turns    uint
+	children []*PathNode
 }
+
+type Path = []Position
 
 // Globals -----------------------------------------------------------
 var Directions = [4]int{North, South, West, East}
+
+const dataPath = "16.txt"
 
 // Functions ---------------------------------------------------------
 func parseData(path string) GameSetup {
@@ -72,10 +76,6 @@ func parseData(path string) GameSetup {
 	return output
 }
 
-func scoreNode(s SearchNode) uint {
-	return s.moves + 1000*s.turns
-}
-
 func Move(p Position, d int) Position {
 	switch d {
 	case North:
@@ -89,22 +89,6 @@ func Move(p Position, d int) Position {
 	default:
 		panic("Bad direction specified")
 	}
-}
-func MoveHeading(h Heading) Position {
-	return Move(h.position, h.direction)
-}
-
-func getPossibleNextNodes(walls map[Position]bool, n SearchNode) []SearchNode {
-	var output []SearchNode
-	for _, dirNext := range Directions {
-		posNext := Move(n.heading.position, dirNext)
-		if !walls[posNext] {
-			headingNext := Heading{position: posNext, direction: dirNext}
-			nodeNext := SearchNode{heading: headingNext, moves: n.moves + 1, turns: n.turns + turns(n.heading.direction, dirNext)}
-			output = append(output, nodeNext)
-		}
-	}
-	return output
 }
 
 func turns(from, to int) uint {
@@ -120,51 +104,135 @@ func turns(from, to int) uint {
 	}
 }
 
+func score(p PathNode) uint {
+	return p.moves + 1000*p.turns
+}
+
+func getMinScoreAt(p Position, s map[Heading]uint) (uint, bool) {
+	atLeastOneScoreRegistered := false
+	var outScore uint = 0
+
+	for dir := range Directions {
+		score, found := s[Heading{position: p, direction: dir}]
+		switch {
+		case found && !atLeastOneScoreRegistered:
+			outScore = score
+			atLeastOneScoreRegistered = true
+		case found:
+			outScore = min(score, outScore)
+		}
+	}
+	return outScore, atLeastOneScoreRegistered
+}
+
+func getPathChildren(p PathNode, walls map[Position]bool) []*PathNode {
+	var nextPaths []*PathNode
+	for _, dirNext := range Directions {
+		posNext := Move(p.heading.position, dirNext)
+		if !walls[posNext] {
+			nextPath := PathNode{}
+			nextPath.heading.position = posNext
+			nextPath.heading.direction = dirNext
+			nextPath.moves = p.moves + 1
+			nextPath.turns = p.turns + turns(p.heading.direction, dirNext)
+			nextPaths = append(nextPaths, &nextPath)
+		}
+	}
+	return nextPaths
+}
+
+func collectPaths(node *PathNode, path *Path, paths *[]Path, endPos Position, endScore uint) {
+	currPos := node.heading.position
+	*path = append(*path, currPos)
+	if currPos == endPos && score(*node) == endScore {
+		correctPath := make([]Position, len(*path))
+		copy(correctPath, *path)
+		*paths = append(*paths, correctPath)
+	} else {
+		for _, child := range node.children {
+			collectPaths(child, path, paths, endPos, endScore)
+		}
+	}
+	*path = (*path)[:len(*path)-1]
+}
+
+func getBestPathPositions(p *PathNode, endPos Position, endScore uint) uint {
+	var pathsToEnd [][]Position
+	var seenPositions = make(map[Position]bool)
+	var positionCount uint = 0
+	var tempPath Path = Path{}
+
+	collectPaths(p, &tempPath, &pathsToEnd, endPos, endScore)
+
+	for _, path := range pathsToEnd {
+		for _, pos := range path {
+			_, posSeen := seenPositions[pos]
+			if !posSeen {
+				positionCount++
+				seenPositions[pos] = true
+			}
+		}
+	}
+	return positionCount
+}
+
 // Main --------------------------------
 func main() {
-	gameSetup := parseData("16.txt")
-	searchNodes := []SearchNode{
-		{heading: Heading{position: gameSetup.start, direction: East},
-			moves: 0,
-			turns: 0}}
+	gameSetup := parseData(dataPath)
+	var pathTree PathNode
+	pathTree.heading.direction = East
+	pathTree.heading.position = gameSetup.start
+	currentSearchNodes := []*PathNode{&pathTree}
+	nextSearchNodes := []*PathNode{}
+	scores := make(map[Heading]uint)
+	scores[pathTree.heading] = 0
+	var endScore uint = 0
+	endScoreFound := false
 
-	nextSearchNodes := []SearchNode{}
-	scores := make(map[Position]uint)
-	scores[gameSetup.start] = 0
-
+	// build the tree
 	for {
+		endScore, endScoreFound = getMinScoreAt(gameSetup.end, scores)
 		// if there's no new positions, we're done
-		if len(searchNodes) == 0 {
+		if len(currentSearchNodes) == 0 {
 			break
 		}
 		// iterate through the positions we're at currently
-		for _, currentNode := range searchNodes {
+		for _, currentNode := range currentSearchNodes {
 			// get the score we're at currently
 			// get the next possible headings
-			nextPossibleNodes := getPossibleNextNodes(gameSetup.walls, currentNode)
+			currentNode.children = getPathChildren(*currentNode, gameSetup.walls)
 			// fmt.Println(currentHeading, "\t", nextPossibleHeadings)
 			// iterate through the next possible headings
-			for _, nextPossibleNode := range nextPossibleNodes {
+			for _, nextPossibleNode := range currentNode.children {
 				// score the heading
-				scoreNext := scoreNode(nextPossibleNode)
+				scoreNext := score(*nextPossibleNode)
 
-				scoreNextAlready, scoredAlreadyNext := scores[nextPossibleNode.heading.position]
-				if !scoredAlreadyNext || scoreNext < scoreNextAlready {
-					scores[nextPossibleNode.heading.position] = scoreNext
+				previouslyRecordedScore, scoredAlready := scores[nextPossibleNode.heading]
+
+				scoredHigherThanEnd := endScoreFound && endScore < scoreNext
+				scoredHigherThanPrevious := scoredAlready && previouslyRecordedScore < scoreNext
+				if !(scoredHigherThanEnd || scoredHigherThanPrevious) {
+					scores[nextPossibleNode.heading] = scoreNext
 					nextSearchNodes = append(nextSearchNodes, nextPossibleNode)
 				}
 			}
 
 		}
-		searchNodes = nextSearchNodes
-		nextSearchNodes = []SearchNode{}
+		currentSearchNodes = nextSearchNodes
+		nextSearchNodes = []*PathNode{}
 	}
 
 	// Part 1
-	f := func(x, y int) {
-		p := Position{x, y}
-		fmt.Println(p, scores[p])
+
+	solution1, solution1Found := getMinScoreAt(gameSetup.end, scores)
+	if solution1Found {
+		fmt.Println("Part 1: ", solution1)
+	} else {
+		fmt.Println("Part1: No solution found")
 	}
-	fmt.Println("Part 1: ", scores[gameSetup.end])
+
+	// Part 2
+	solution2 := getBestPathPositions(&pathTree, gameSetup.end, solution1)
+	fmt.Println("Part 2: ", solution2)
 
 }
