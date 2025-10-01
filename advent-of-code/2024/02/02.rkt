11b8da1493ada772d25329022235189542e64c4b
Red Swan <redswan@outlook.com>
2025-10-01 00:50:38 -0600

    Solve advent 2024-02

--- advent-of-code/2024/02/02.rkt
+++ advent-of-code/2024/02/02.rkt
@@ -1,69 +1,73 @@
-#lang racket
+#lang 2d racket
 
+(require 2d/cond)
 
 (define (sign x)
   (cond
     [(< x 0) 'negative]
     [(< 0 x) 'positive]
     [else    'zero]))
-                
-(define (cumdiff xs)
-  (let* ([n (length xs)]
-         [k (- n 1)])
-    (map - (rest xs) (take xs k))))
 
 (define (between a b x)
   (and
    (<= a x)
    (<= x b)))
 
-(define (safe-diff? x dir)
-  (match dir
-    ['increasing (between 1 3 x)]
-    ['decreasing (between 1 3 x)]
-    [-1          (safe-diff? x 'decreasing)]
-    [ 1          (safe-diff? x 'increasing)]
-    [else        #f]))
+(define (safe-report? report [skip #f])
+  (define (loop prev-x xs i prev-dir)
+    (cond
+      [(empty? xs) #t]
+      [(and skip (= i skip)) (loop prev-x (rest xs) (add1 i) prev-dir)]
+      [else (let* ([x (first xs)]
+                   [diff (- x prev-x)]
+                   [dir (sign diff)])
+            #2dcond
+  ╔══════════════════════════╦═══════════════════════╦═══════════════════════╦═════════════════╗
+  ║                          ║   (not prev-dir)      ║ (equal? prev-dir dir) ║ else            ║
+  ╠══════════════════════════╬═══════════════════════╩═══════════════════════╬═════════════════╣
+  ║ (between 1 3 (abs diff)) ║             (loop x (rest xs) (add1 i) dir)   ║                 ║ 
+  ╠══════════════════════════╬═══════════════════════════════════════════════╝                 ║
+  ║ else                     ║                  #f                                             ║
+  ╚══════════════════════════╩═════════════════════════════════════════════════════════════════╝ )]))
+  (if (and skip (zero? skip))
+      (loop (second report)  (rest (rest report)) 1 #f)
+      (loop (first report) (rest report) 1 #f)))
+  
 
+         
+(define (safe-report2? report)
+  (define (loop [i 0] [max (length report)])
+    (cond
+      [(= i max) #f]
+      [(safe-report? report i) #t]
+      [else (loop (add1 i) max)]))
+  (loop))
 
-(define (safe-report? report [tolerance 0])
-  (let ([diffs (cumdiff report)])
-    (let-values ([(errors dir)]
-                 (for/fold ([errors 0]
-                            [dir #f])
-                           ([d diffs])
-                   (if dir
-                       (if (safe-diff? d dir)
-                           (values errors dir)
-                           (values (add1 errors) dir))
-                       (values errors (sign d)))))
-      (<= errors tolerance))))
-             
-    
+; Read Data ------------------------------------------------------------------
 
-; Read Data
-
-(define reports
-  (with-input-from-file "02-sample.txt"
-    (λ ()
-      (for/list ([line (in-lines)])
-        (line->report line)))))
+(define input-path (make-parameter "02-sample.txt"))
+(input-path "02.txt")
 
 (define (line->report line)
   (map string->number
        (string-split line " ")))
 
+(define reports
+  (with-input-from-file (input-path)
+    (λ ()
+      (for/list ([line (in-lines)])
+        (line->report line)))))
 
-; Part 1
+
+
+; Part 1 --------------------------------------------------------------------
+
+(count safe-report? reports)
 
 
 ; Part 2
 
-
-; Scratch
-
-(define asd (map cumdiff reports))
-(map count-sign-switches asd)
+(count safe-report2? reports)
 
 
 
