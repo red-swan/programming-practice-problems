7bf1c4fb231ebb546ea1b718c9e00988a9a4983b
Red Swan <redswan@outlook.com>
2025-10-08 22:07:47 -0600

    Add pattern building

--- advent-of-code/2024/04/04.rkt
+++ advent-of-code/2024/04/04.rkt
@@ -1,5 +1,6 @@
 #lang racket
 
+(require racket/hash)
 
 ;   x ->->->
 ; y 0,0  1,0
@@ -9,26 +10,26 @@
 ; | 0,2  1,2
 ; v
 
-(define DEV #f)
+(define DEV #t)
 (define input-path (make-parameter "04.txt"))
 (when DEV (input-path "04-sample.txt"))
 
 ; build a mapping from coordinate to letter
 (define (build-search-mapping path)
   (for/fold ([search (hash)]
-             [x 0]
+             [y 0]
              #:result search)
             ([line (file->lines path)])
     (for/fold ([search search]
-               [y 0]
-               #:result (values search (add1 x)))
+               [x 0]
+               #:result (values search (add1 y)))
               ([char (string->list line)])
       (values
        (hash-set search (list x y) char)
-       (add1 y)))))
+       (add1 x)))))
 
 ; Build directions and move from one to another
-(define dirs '(N S E W NE NW SW SE))
+(define directions '(N S E W NE NW SW SE))
 (define (step from dir)
   (match-let ([(list x y) from])
     (match dir
@@ -51,7 +52,7 @@
 
 ; Count all occurrences of a word at a location in all directions
 (define (count-occurrences-at word-search index word [dir #f])
-  (count identity (map (curry has-word? word-search index word) dirs)))
+  (count identity (map (curry has-word? word-search index word) directions)))
 
 ; Count all occurrences of the word in a word search
 (define (count-occurrences word-search word)
@@ -66,6 +67,59 @@
 ; Part 1 ----------------------------------------------------
 
 (count-occurrences word-search "XMAS")
+
+
+
+; Scratch ---------------------------------------------------
+
+(define (add pt1 pt2)
+  (match-let ([(list x1 y1) pt1]
+              [(list x2 y2) pt2])
+    (list (+ x1 x2) (+ y1 y2))))
+
+(define (pattern-at? word-search at pattern)
+  (let* ([n (hash-count pattern)]
+         [pattern* (hash-map pattern (λ (key val) (add at val)))]
+         [found (hash-intersect word-search pattern*)])
+    (= n (hash-count found))))
+    
+
+
+
+
+
+
+
+
+; 5 0
+
+(define (build-word-pattern dir word)
+  (for/fold ([at (list 0 0)]
+             [indices (hash)]
+             #:result indices)
+            ([char (string->list word)])
+    (values
+     (step at dir)
+     (hash-set indices at char))))
+
+(define (build-word-patterns word)
+  (map (curryr build-word-pattern word) directions))
+
+
+(define asd (build-word-patterns "XMAS"))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
