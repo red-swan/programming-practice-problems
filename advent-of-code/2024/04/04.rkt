0a38fa4c8aa8efe898767dccc2587c035b58009f
Red Swan <redswan@outlook.com>
2025-10-10 01:27:24 -0600

    Solve part 1 sample using generic pattern checker

--- advent-of-code/2024/04/04.rkt
+++ advent-of-code/2024/04/04.rkt
@@ -10,7 +10,7 @@
 ; | 0,2  1,2
 ; v
 
-(define DEV #t)
+(define DEV #f)
 (define input-path (make-parameter "04.txt"))
 (when DEV (input-path "04-sample.txt"))
 
@@ -42,6 +42,7 @@
       ['SW (list (sub1 x) (add1 y))]
       ['SE (list (add1 x) (add1 y))])))
 
+#|
 ; Search for word at a location in a direction
 (define (has-word? word-search at word dir)
   (cond
@@ -59,43 +60,51 @@
   (for/fold ([count 0])
             ([index (hash-keys word-search)])
     (+ count (count-occurrences-at word-search index word))))
-
+|#
 ; Read data -------------------------------------------------
 
 (define word-search (build-search-mapping (input-path)))
     
 ; Part 1 ----------------------------------------------------
 
-(count-occurrences word-search "XMAS")
+;(count-occurrences word-search "XMAS")
 
 
 
 ; Scratch ---------------------------------------------------
+
+
+; Add x y coordinates stored as lists
 
 (define (add pt1 pt2)
   (match-let ([(list x1 y1) pt1]
               [(list x2 y2) pt2])
     (list (+ x1 x2) (+ y1 y2))))
 
-(define (hash-update-all ht updater)
+; Hash functions
+
+(define (hash-update-keys ht updater)
   (for/hash ([k (hash-keys ht)])
-    (values k (updater (hash-ref ht k)))))
+    (values (updater k) (hash-ref ht k))))
 
-(define (pattern-at? word-search at pattern)
-  (let* ([n (hash-count pattern)]
-         [pattern* (hash-update-all pattern (curry add at))]
-         [found (hash-intersect word-search pattern*)])
-    (= n (hash-count found))))
-    
+(define (hash-intersection ht1 ht2)
+  (for/hash ([key1 (hash-keys ht1)]
+             #:when (and (hash-has-key? ht2 key1)
+                     (equal? (hash-ref ht1 key1)
+                             (hash-ref ht2 key1))))
+    (values key1 (hash-ref ht1 key1))))
 
+(define (hash-subset? ht ht2)
+  (for/fold ([still-subset? #t])
+            ([key2 (hash-keys ht2)])
+    (if still-subset?
+        (and (hash-has-key? ht key2)
+             (equal? (hash-ref ht key2)
+                     (hash-ref ht2 key2)))
+        still-subset?)))
+            
 
-
-
-
-
-
-
-; 5 0
+; Building patterns for words in a direction
 
 (define (build-word-pattern dir word)
   (for/fold ([at (list 0 0)]
@@ -111,15 +120,34 @@
 
 
 
-(define (count/word-search word-search patterns)
-  (for/fold ([occurrences 0])
-             ([pos (hash-keys word-search)])
-    (+ occurrences
-       (count identity (map (curry pattern-at? word-search pos) patterns)))))
-             
 
-             
-(count/word-search word-search (build-word-patterns "XMAS"))
+; Searching for patterns in a word search
+
+(define (pattern-at? word-search at pattern)
+  (let* ([n (hash-count pattern)]
+         [pattern* (hash-update-keys pattern (curry add at))]
+         [found (hash-intersection word-search pattern*)])
+    (= n (hash-count found))))
+    
+(define (find-occurrences word-search pattern)
+  (for/fold ([occurrences (set)])
+            ([at (hash-keys word-search)])
+    (let ([pattern* (hash-update-keys pattern (curry add at))])
+      
+    (if (hash-subset? word-search pattern*)
+        (set-add occurrences pattern*)
+        occurrences))))
+        
+(define (count-occurrences word-search patterns)
+  (set-count
+   (foldl set-union
+          (set)
+          (map (curry find-occurrences word-search) patterns))))
+
+
+
+(define xmas-patterns (build-word-patterns "XMAS"))
+(count-occurrences word-search xmas-patterns)
 
 
 
