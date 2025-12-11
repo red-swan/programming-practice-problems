1559bc47600bb0a3dbedcf25139912401f700cd3
Red Swan <redswan@outlook.com>
2025-12-11 07:26:27 -0700

    Add rework

--- advent-of-code/2025/01/01.rkt
+++ advent-of-code/2025/01/01.rkt
@@ -1,22 +1,37 @@
 #lang racket
 
 
-(define (turn dir from)
-  (match-let ([(list _ dir amount) (regexp-match #px"^(L|R)(\\d+)" dir)])
-    (let ([amount (string->number amount)]
-          [f (if (equal? dir "L") - +)])
-      (modulo (f from amount) 100))))
+; Functions -------------------------------------------------------------
+
+(define (string->instruction s)
+  (match-let ([(list _ dir amount) (regexp-match #px"^(L|R)(\\d+)" s)])
+    (list
+     (if (equal? dir "L") 'L 'R)
+     (string->number amount))))
+      
+
+(define (turn instruction from [normalize #t])
+  (match-let ([(list dir amount) instruction])
+    
+    [(list 'L n) (- from n)]
+    [(list 'R n) (+ from n)]
+    [_ (error "Invalid instruction")]))
+    
+    
+
+; Reading Data ----------------------------------------------------------
+(define instructions (map string->instruction (file->lines "sample.txt")))
+
+(foldl turn 50 instructions)
 
 
-(define turns (file->lines "input.txt"))
 (define codes
   (for/fold ([acc '(50)]
              [x 50]
              #:result (reverse acc))
-            ([instruction turns])
+            ([instruction instructions])
     (let ([x* (turn instruction x)])
       (values (cons x* acc) x*))))
 
-
-; Part 1
-(count zero? codes)
+; Part 1 ----------------------------------------------------------------
+; (count zero? codes)
