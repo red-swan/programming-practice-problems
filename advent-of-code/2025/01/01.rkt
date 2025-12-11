001acee2cd663e9704109202be60c488c4de765e
Red Swan <redswan@outlook.com>
2025-12-11 10:53:36 -0700

    Finish 2025-01

--- advent-of-code/2025/01/01.rkt
+++ advent-of-code/2025/01/01.rkt
@@ -8,30 +8,54 @@
     (list
      (if (equal? dir "L") 'L 'R)
      (string->number amount))))
-      
 
-(define (turn instruction from [normalize #t])
-  (match-let ([(list dir amount) instruction])
-    
-    [(list 'L n) (- from n)]
-    [(list 'R n) (+ from n)]
-    [_ (error "Invalid instruction")]))
-    
-    
+
+(define (count-zeroes tick dir amount)
+  (let-values ([(q r) (quotient/remainder amount 100)])
+    (if (and
+         ((negate zero?) tick)
+         (or
+          (and (equal? 'L dir)
+               (<= tick r))
+          (and (equal? 'R dir)
+               (<= (- 100 tick) r))))
+        (add1 q)
+        q)))
+
+(define (turn tick dir amount)
+  (let* ([f (case dir ['L -] ['R +])]
+         [result (f tick amount)]
+         [tick* (modulo result 100)])
+    (values
+     ; new tick
+     tick*
+     ; tick* on zero?
+     (if (zero? tick*) 1 0)
+     ; touched zero?
+     (count-zeroes tick dir amount))))
+
+
+
 
 ; Reading Data ----------------------------------------------------------
-(define instructions (map string->instruction (file->lines "sample.txt")))
 
-(foldl turn 50 instructions)
+(define target-path "input.txt")
 
 
-(define codes
-  (for/fold ([acc '(50)]
-             [x 50]
-             #:result (reverse acc))
-            ([instruction instructions])
-    (let ([x* (turn instruction x)])
-      (values (cons x* acc) x*))))
+(define-values (final-tick part1 part2)
+  (for/fold ([tick 50]
+             [zero-lands 0]
+             [zero-touches 0])
+            ([line (file->lines target-path)])
+    (match-let ([(list dir amount) (string->instruction line)])
+      (let-values ([(tick* dl dt) (turn tick dir amount)])
+        (values
+         tick*
+         (+ zero-lands dl)
+         (+ zero-touches dt))))))
 
-; Part 1 ----------------------------------------------------------------
-; (count zero? codes)
+; Part 1
+part1
+
+; Part 2
+part2
